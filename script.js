65e1b0661710197f009d52e732e4072933992c73
vanshika022005 <vanshika.25b01010366@abes.ac.in>
2026-02-27 00:14:27 +0530

    reference results

--- script.js
+++ script.js
@@ -79,38 +79,62 @@
 //         console.log(linebreak); 
 
 
-var a = 10; 
-        var b = 20; 
-        var linebreak = "<br />"; 
+// var a = 10; 
+//         var b = 20; 
+//         var linebreak = "<br />"; 
  
-        console.log("(a == b) => "); 
-        result = (a == b); 
-        console.log(result); 
-        console.log(linebreak); 
+//         console.log("(a == b) => "); 
+//         result = (a == b); 
+//         console.log(result); 
+//         console.log(linebreak); 
  
-        console.log("(a < b) => "); 
-        result = (a < b); 
-        console.log(result); 
-        console.log(linebreak); 
+//         console.log("(a < b) => "); 
+//         result = (a < b); 
+//         console.log(result); 
+//         console.log(linebreak); 
  
-        console.log("(a > b) => "); 
-        result = (a > b); 
-        console.log(result); 
-        console.log(linebreak); 
+//         console.log("(a > b) => "); 
+//         result = (a > b); 
+//         console.log(result); 
+//         console.log(linebreak); 
  
-        console.log("(a != b) => "); 
-        result = (a != b); 
-        console.log(result); 
-        console.log(linebreak); 
+//         console.log("(a != b) => "); 
+//         result = (a != b); 
+//         console.log(result); 
+//         console.log(linebreak); 
  
-        console.log("(a >= b) => "); 
+//         console.log("(a >= b) => "); 
          
-        result = (a >= b); 
-        console.log(result); 
-        console.log(linebreak); 
+//         result = (a >= b); 
+//         console.log(result); 
+//         console.log(linebreak); 
  
-        console.log("(a <= b) => "); 
-        result = (a <= b); 
-        console.log(result); 
-        console.log(linebreak); 
+//         console.log("(a <= b) => "); 
+//         result = (a <= b); 
+//         console.log(result); 
+//         console.log(linebreak); 
      
+
+var a = 40; 
+           var b = 20; 
+           var linebreak = "<br />"; 
+         
+           console.log("Value of a => (a = b) => "); 
+           result = (a = b); 
+           console.log(result); 
+           console.log(linebreak); 
+         
+           console.log("Value of a => (a += b) => "); 
+           result = (a += b); 
+           console.log(result); 
+           console.log(linebreak); 
+         
+           console.log("Value of a => (a -= b) => "); 
+           result = (a -= b); 
+           console.log(result); 
+           console.log(linebreak); 
+         
+           console.log("Value of a => (a *= b) => "); 
+           result = (a *= b); 
+           console.log(result); 
+           console.log(linebreak);
