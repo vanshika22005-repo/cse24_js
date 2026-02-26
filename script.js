acded1be4b084ba8f553b8771784c0527df75568
vanshika022005 <vanshika.25b01010366@abes.ac.in>
2026-02-27 00:05:00 +0530

    symbol

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 // console.log("Hello, World!");
 // document.write("hello world");
 
- var num=10;
-    console.log(num);
-    console.log(typeof(num));
+//  var num=10;
+//     console.log(num);
+//     console.log(typeof(num));
     
-    num=false;
+//     num=false;
 
-    //function
-    var sayhello=function(){
-        alert("hello world");
+//     //function
+//     var sayhello=function(){
+//         alert("hello world");
+//     }
+
+//     //array
+//     var numberarray=[1,2,3];
+//     var animals=new Array("cat","dog","mouse","lion");
+
+//     //object
+//     var person={
+//         name:"barack obamaII",
+//         age:'51',
+//         title:'44th president of usa'
+//     }
+
+const sym1=Symbol(4);
+    console.log(sym1);
+    const sym2=Symbol(4);
+    console.log(sym2);
+    if(sym1==sym2){
+        console.log("true");
     }
-
-    //array
-    var numberarray=[1,2,3];
-    var animals=new Array("cat","dog","mouse","lion");
-
-    //object
-    var person={
-        name:"barack obamaII",
-        age:'51',
-        title:'44th president of usa'
+    else{
+        console.log("false");
     }
-    
