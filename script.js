1895bf83d955bdbb38bcb3c2f4f0931b71adf702
vanshika022005 <vanshika.25b01010366@abes.ac.in>
2026-02-27 00:03:57 +0530

    reference object

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-console.log("Hello, World!");
-document.write("hello world");
+// console.log("Hello, World!");
+// document.write("hello world");
+
+ var num=10;
+    console.log(num);
+    console.log(typeof(num));
+    
+    num=false;
+
+    //function
+    var sayhello=function(){
+        alert("hello world");
+    }
+
+    //array
+    var numberarray=[1,2,3];
+    var animals=new Array("cat","dog","mouse","lion");
+
+    //object
+    var person={
+        name:"barack obamaII",
+        age:'51',
+        title:'44th president of usa'
+    }
+    
