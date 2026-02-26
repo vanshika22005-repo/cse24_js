e6557c211436d7aca010c710d262681252152a17
vanshika022005 <vanshika.25b01010366@abes.ac.in>
2026-02-27 00:08:42 +0530

    typeof

--- script.js
+++ script.js
@@ -23,13 +23,29 @@
 //         title:'44th president of usa'
 //     }
 
-const sym1=Symbol(4);
-    console.log(sym1);
-    const sym2=Symbol(4);
-    console.log(sym2);
-    if(sym1==sym2){
-        console.log("true");
-    }
-    else{
-        console.log("false");
-    }
+// const sym1=Symbol(4);
+//     console.log(sym1);
+//     const sym2=Symbol(4);
+//     console.log(sym2);
+//     if(sym1==sym2){
+//         console.log("true");
+//     }
+//     else{
+//         console.log("false");
+//     }
+
+var num = 50; 
+console.log(num); 
+console.log(typeof(num)); 
+num = false; 
+console.log(num); 
+console.log(typeof(num)); 
+num = "Raj Singh"; 
+console.log(num); 
+console.log(typeof(num)); 
+num = null; 
+console.log(num); 
+console.log(typeof(num)); 
+num = undefined; 
+console.log(num); 
+console.log(typeof(num));
